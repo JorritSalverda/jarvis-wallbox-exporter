0e956063045f7642ddec41e7feabab21875bebe9
Jorrit Salverda <jorrit.salverda@gmail.com>
2022-01-06 11:46:46 +0100

    send measurement to nats instead of bigquery

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod wallbox_api_client;
 mod model;
 
 use wallbox_api_client::{WallboxApiClient, WallboxApiClientConfig};
-use jarvis_lib::bigquery_client::{BigqueryClient, BigqueryClientConfig};
+use jarvis_lib::nats_client::{NatsClient, NatsClientConfig};
 use jarvis_lib::config_client::{ConfigClient, ConfigClientConfig};
 use jarvis_lib::exporter_service::{ExporterService, ExporterServiceConfig};
 use jarvis_lib::state_client::{StateClient, StateClientConfig};
@@ -15,15 +15,15 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let state_client_config = StateClientConfig::from_env().await?;
     let state_client = StateClient::new(state_client_config);
 
-    let bigquery_client_config = BigqueryClientConfig::from_env().await?;
-    let bigquery_client = BigqueryClient::new(bigquery_client_config);
+    let nats_client_config = NatsClientConfig::from_env().await?;
+    let nats_client = NatsClient::new(nats_client_config);
 
     let config_client_config = ConfigClientConfig::from_env()?;
     let config_client = ConfigClient::new(config_client_config);
 
     let exporter_service_config = ExporterServiceConfig::new(
         config_client,
-        bigquery_client,
+        nats_client,
         state_client,
         Box::new(wallbox_api_client),
     )?;
